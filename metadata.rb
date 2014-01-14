5527a7c1ebe40bb2a8946e6b3b5de5b87231eaa8
Sean Porter <portertech@gmail.com>
2014-01-13 21:57:44 -0800

    version bump, 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "ops@hw-ops.com"
 license          "Apache 2.0"
 description      "Installs/Configures graphiti"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.1.0"
 
 depends "build-essential"
 depends "ruby_installer"
